242dd637f49872d221423684b243a884450ea495
Mark Hamstra <hello@markhamstra.com>
2019-08-15 15:39:35 +0200

    Sync translations

---
+++ core/components/commerce_pdfcrowdwriter/lexicon/cs/default.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$_lang['commerce_pdfcrowdwriter'] = 'PDF Writer: PDFCrowd';
+$_lang['commerce_pdfcrowdwriter.description'] = 'When configured with a valid username and API Key, this will enable a PDFCrowd Writer. ';
+$_lang['commerce_pdfcrowdwriter.username'] = 'PDFCrowd Username';
+$_lang['commerce_pdfcrowdwriter.username.desc'] = 'The username you use to login to your PDFCrowd account.';
+$_lang['commerce_pdfcrowdwriter.apikey'] = 'PDFCrowd API Key (v2)';
+$_lang['commerce_pdfcrowdwriter.apikey.desc'] = 'The API Key for your v2 license. You can find this under Licenses > API v2 License.';
