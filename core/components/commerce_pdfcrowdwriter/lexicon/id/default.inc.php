eb9a5feb4b9de9cc3e3c4e22ee701680d2b41e0c
Mark Hamstra <hello@markhamstra.com>
2021-07-26 18:14:31 +0200

    Lexicon sync

--- core/components/commerce_pdfcrowdwriter/lexicon/id/default.inc.php
+++ core/components/commerce_pdfcrowdwriter/lexicon/id/default.inc.php
@@ -6,3 +6,5 @@ $_lang['commerce_pdfcrowdwriter.username'] = 'PDFCrowd Username';
 $_lang['commerce_pdfcrowdwriter.username.desc'] = 'The username you use to login to your PDFCrowd account.';
 $_lang['commerce_pdfcrowdwriter.apikey'] = 'PDFCrowd API Key (v2)';
 $_lang['commerce_pdfcrowdwriter.apikey.desc'] = 'The API Key for your v2 license. You can find this under Licenses > API v2 License.';
+$_lang['commerce_pdfcrowdwriter.converter'] = 'Converter version';
+$_lang['commerce_pdfcrowdwriter.converter.desc'] = 'PDFCrowd supports two different converters: 20.10 and 18.10. For the v1 integration, 18.10 was the default. In the current release, 20.10 is the default. If your PDFs look different after the update, you can switch back to the old version.';
